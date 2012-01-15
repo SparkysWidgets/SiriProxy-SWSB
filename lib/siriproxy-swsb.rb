451f7333d6d2ce52b43209ae0e4087e0da4fb961
Sparky's Widgets <SparkysWidgets@gmail.com>
2012-01-15 01:45:04 -0800

    changes to use for lighting

--- lib/siriproxy-swsb.rb
+++ lib/siriproxy-swsb.rb
@@ -45,8 +45,20 @@ listen_for /test smart box/i do
 
 #listen_for(/turn on light.*([0-9]+\s*|one|two|three|four|five|six|seven|eight|nine|ten)/i) { |number| turn_on_light(number) } 
 #listen_for(/turn off light.*([0-9]+\s*|one|two|three|four|five|six|seven|eight|nine|ten)/i) { |number| turn_off_light(number) } 
-listen_for(/turn on all lights/i) { all_lights_on()}
-listen_for(/turn off all lights)/i) { all_lights_off()} 
+
+listen_for/turn on all lights/i do
+  sp = SerialPort.new(@comport, @baudrate, @databits, @stopbits, @parity)
+    say "turning on all lab lights"
+    sp.write "CAH"
+   request_completed #always complete your request! Otherwise the phone will "spin" at the user!
+  end
+
+listen_for /turn off all lights)/i do
+  sp = SerialPort.new(@comport, @baudrate, @databits, @stopbits, @parity)
+    say "turning off all lab lights"
+    sp.write "CAL"
+   request_completed #always complete your request! Otherwise the phone will "spin" at the user!
+  end
 
 #def turn_on_light(number)
 #     sp = SerialPort.new(@comport, @baudrate, @databits, @stopbits, @parity)
@@ -85,19 +97,5 @@ listen_for(/turn off all lights)/i) { all_lights_off()}
 #    else
 #   request_completed #always complete your request! Otherwise the phone will "spin" at the user!
 #  end
-
-   def all_lights_on()
-    sp = SerialPort.new(@comport, @baudrate, @databits, @stopbits, @parity)
-    say "turning on all lab lights"
-    sp.write "CAH"
-   request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-  end
-
-   def all_lights_on()
-    sp = SerialPort.new(@comport, @baudrate, @databits, @stopbits, @parity)
-    say "turning off all lab lights"
-    sp.write "CAL"
-   request_completed #always complete your request! Otherwise the phone will "spin" at the user!
-  end
  
 end
